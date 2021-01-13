0b0073bf4f87ea7fb29b7e2e102f426af6969fc6
Pierre Lebrun <anthonylebrun@gmail.com>
2021-01-13 14:54:37 -0500

    Added notes to examples

--- app/javascript/example_2.vue
+++ app/javascript/example_2.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="space-y-6">
+    <div class="space-y-6">
+      <div class="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <!-- Heroicon name: exclamation -->
+            <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+            </svg>
+          </div>
+          <div class="ml-3">
+            <p class="text-sm text-yellow-700">
+              This page does not validate the website URL before using it in an <code>a#href</code> attribute.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <h1 class="text-3xl">2. XSS vue template</h1>
     <div id="profile-form" class="space-y-6">
       <div>
@@ -17,7 +35,7 @@
       <div>
         <label for="website" class="block text-sm font-medium text-gray-700">Website</label>
         <div class="mt-1">
-          <input v-model="website" id="website" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="http://example.com">
+          <input v-model="website" id="website" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="http://example.com">
         </div>
       </div>
       <button @click="showProfile = true" type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
